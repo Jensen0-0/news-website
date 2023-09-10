c55ac3cff02401261d1386372010e2c5d37242b0
Memphis <138075773+mMemphis@users.noreply.github.com>
2023-09-10 02:43:04 +0200

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,1 +1,19 @@
-export default function Home() { }
+import Image from "next/image"
+import ZMLogo from "../assets/branding/ZM_logo.png"
+export default function Info({ posts }) {
+    return (
+        <div className="">
+            <div className="m-auto p-10 w-full">
+            <b>  Voor uw eigen nieuws pagina kan u uw zelf nu nog aanmelding in de gemeente app onder de categorie Sollicitatie's </b>
+            </div>
+
+
+
+
+            {/* <div className="m-auto w-10 h-10 bg-slate-50">
+
+            </div> */}
+        </div>
+
+    )
+}
